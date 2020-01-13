cff33c3f7fced51726adc992f6466f80d0277958
Tonalli <tonalli@xanum.uam.mx>
2020-01-12 22:01:38 -0600

    Add dynamic url

--- headlines.py
+++ headlines.py
@@ -1,17 +1,23 @@
 import feedparser
 from flask import Flask
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+BBC_FEED = ""
 
 app = Flask(__name__)
 
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return f"""<html>
     <body>
-        <h1> BBC Headlines </h1>
+        <h1> Headlines </h1>
         <b>{first_article.get('title')}</b> <br>
         <i>{first_article.get('published')}</i> <br>
         <p>{first_article.get('summary')}</p> <br>
